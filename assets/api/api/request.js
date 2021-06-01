5a9b573490ec53bc7fb0578665d733e8812aa748
Dmitrii Kliuev <dmitrii.kliuev.1989@gmail.com>
2021-06-01 22:15:58 +0300

    remove random error in request

--- assets/api/api/request.js
+++ assets/api/api/request.js
@@ -1,23 +1,8 @@
 import vehicles from './vehicles.json'
 
-/** True = 65%, False = 35% */
-const rejectByChance = () => {
-  return Math.random() <= 0.35
-}
-
 /** Emulate request */
 export const getVehicles = () =>
-  new Promise((resolve, reject) => {
-    if (rejectByChance()) {
-      reject(
-        new Error(`
-        Server error
-        Server error
-        Server error
-        Server error
-        Server error`)
-      )
-    }
+  new Promise((resolve) => {
     const delay = parseInt(Math.random() * 1000)
     setTimeout(() => {
       resolve(vehicles)
