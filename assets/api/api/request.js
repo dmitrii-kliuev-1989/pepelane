acce3d9c42b4bf8643bcbd448245ed5920735027
Dmitrii Kliuev <dmitrii.kliuev.1989@gmail.com>
2021-06-03 15:46:50 +0300

    remove response delay

--- assets/api/api/request.js
+++ assets/api/api/request.js
@@ -3,8 +3,5 @@ import vehicles from './vehicles.json'
 /** Emulate request */
 export const getVehicles = () =>
   new Promise((resolve) => {
-    const delay = parseInt(Math.random() * 1000)
-    setTimeout(() => {
-      resolve(vehicles)
-    }, delay)
+    resolve(vehicles)
   })
